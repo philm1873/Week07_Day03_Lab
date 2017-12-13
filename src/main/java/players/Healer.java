770bd8e9188dc7d7f8cd42da64224bd0748edcc2
Phil Mitchell <pm1873@gmail.com>
2017-12-13 12:04:56 +0000

    added Healer methods

--- src/main/java/players/Healer.java
+++ src/main/java/players/Healer.java
@@ -1,4 +1,24 @@
 package players;
 
-public class Healer {
+public class Healer extends Player{
+    
+    int potions;
+
+    public Healer(String name, int health, int potions) {
+        super(name, health);
+        this.potions = potions;
+    }
+
+    public void reducePotions(){
+        this.potions--;
+    }
+    
+    public int heal(){
+        reducePotions();
+        return 50;
+    }
+
+    public int getPotions() {
+        return potions;
+    }
 }
--- src/test/java/HealerTest.java
+++ src/test/java/HealerTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+import players.Healer;
+
+import static org.junit.Assert.assertEquals;
+
 public class HealerTest {
+
+    Healer testHealer;
+
+    @Before
+    public void before(){
+        testHealer = new Healer("Alchemist", 1000, 10);
+    }
+
+    @Test
+    public void canHeal() {
+        assertEquals(50, testHealer.heal());
+        assertEquals(9, testHealer.getPotions());
+    }
+
 }
